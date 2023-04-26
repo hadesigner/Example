b419e3f22417f7a31706c8b61bc34e9c798112cf
gk019 <gk019@MintZero>
2023-04-26 16:42:50 +0900

    Example 0426

---
+++ Example/src/E0426/CharExample.java
@@ -0,0 +1,15 @@
+package E0426;
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		char c1 = 'A';
+		char c2 = 66;
+		int i3 = 65;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(i3);
+	}
+
+}
---
+++ Example/src/E0426/VariableScopeExample3.java
@@ -0,0 +1,22 @@
+package E0426;
+
+public class VariableScopeExample3 {
+
+	public static void main(String[] args) {
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = -0;
+		byte var4 = 30;
+		byte var5 = 127;
+//		byte var6 = 128;
+		byte var6 = (byte) 128;
+	
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+	}
+
+}
